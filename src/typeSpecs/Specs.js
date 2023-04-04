3b110b4a9432a38cfb4d7ddfa19c069d597e78e6
Kevin Talmadge <42550910+elv-kevin@users.noreply.github.com>
2023-04-04 13:32:14 -0700

    Main site spec update (#37)

--- src/typeSpecs/Specs.js
+++ src/typeSpecs/Specs.js
@@ -3,8 +3,10 @@ import TenantSpec from "./EventTenant";
 import NFTCollectionSpec from "./NFTCollection";
 import NFTTemplateSpec from "./NFTTemplate";
 import MarketplaceSpec from "./Marketplace";
+import MainSiteSpec from "./MainSite";
 
 const Specs = {
+  "Main Site": MainSiteSpec,
   "Event Site": EventSiteSpec,
   "Tenant": TenantSpec,
   "NFT Collection": NFTCollectionSpec,
